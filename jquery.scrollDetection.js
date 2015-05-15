26ae9b1ae19fec006793f3b0999d930dc968a22a
melbon <mario@vidov.it>
2015-05-15 19:48:02 +0300

    Added

---
+++ jquery.scrollDetection.js
@@ -0,0 +1,25 @@
+/*
+ * May 2015
+ * scrollDetection 1.0.0
+ * @author Mario Vidov
+ * @url http://vidov.it
+ * @twitter MarioVidov
+ * MIT license
+ */
+$.scrollDetection = function(options) {
+    var settings = $.extend({
+        scrollDown: function() {},
+        scrollUp: function() {}
+    }, options);
+
+    var scrollPosition = 0;
+    $(window).scroll(function () {
+        var cursorPosition = $(this).scrollTop();
+        if (cursorPosition > scrollPosition) {
+            settings.scrollDown();
+        } else {
+            settings.scrollUp();
+        }
+        scrollPosition = cursorPosition;
+    });
+};
